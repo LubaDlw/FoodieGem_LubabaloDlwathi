51475c63e7f2ed1d3ba2528f6772c124e50c779b
LubaDlw <132472444+LubaDlw@users.noreply.github.com>
2025-06-04 19:20:02 +0200

    Update UI - Categories

--- src/utils/categories.js
+++ src/utils/categories.js
@@ -1,18 +1,18 @@
 // categories const
 
 export const categories = [
-  { id: 1, name: "Date Night", image: "../assets/topRated.jpg" },
-  { id: 2, name: "Budget Friendly", image: "../assets/seafood.jpg" },
-  { id: 3, name: "Hidden Gems", image: "../assets/gourmetCat.jpg" },
-  { id: 4, name: "Near You", image: "../assets/asianfood.jpg" },
-  { id: 5, name: "Study / Work Spots", image: "../assets/fastfood.jpg" },
-  { id: 6, name: "Family Friendly", image: "../assets/budgetBites.jpg" },
-  { id: 7, name: "Breakfast / Brunch", image: "../assets/asianfood.jpg" },
-  { id: 8, name: "Fine Dining", image: "../assets/asianfood.jpg" },
+  { id: 1, name: "Date Night", image: "../assets/date-night.png"},
+  { id: 2, name: "Budget Friendly", image: "../assets/budgetBites.jpg" },
+  { id: 3, name: "Hidden Gems", image: "../assets/gem.png" },
+  { id: 4, name: "Near You", image: "../assets/nearYou.jpg" },
+  { id: 5, name: "Study / Work Spots", image: "../assets/mortarboard.png" },
+  { id: 6, name: "Family Friendly", image: "../assets/family.jpg" },
+  { id: 7, name: "Breakfast / Brunch", image: "../assets/breakfast.png" },
+  { id: 8, name: "Fine Dining", image: "../assets/fine-dining.png" },
   { id: 9, name: "Casual Eats / Café", image: "../assets/asianfood.jpg" },
-  { id: 10, name: "Bar & Lounge", image: "../assets/lateNight.jpg" },
-  { id: 11, name: "Ethnic / World Cuisine", image: "../assets/dateNight.jpg" },
-  { id: 12, name: "Healthy / Vegetarian & Vegan", image: "../assets/student.jpg" }
+  { id: 10, name: "Bar & Lounge", image: "../assets/cocktail.jpg" },
+  { id: 11, name: "Ethnic / World Cuisine", image: "../assets/map.png" },
+  { id: 12, name: "Healthy / Vegetarian & Vegan", image: "../assets/healthy.jpg" }
 ];
 
 // Utility function to get random categories
